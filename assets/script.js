6c6f349ee212a87e66a7aceda758371e05a3fdba
Laura Beutler <labeutler@gmail.com>
2022-10-04 19:01:09 -0400

    Started on JavaScript by adding timer code and not working.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
 //countdown timer
+const startingMinutes = 1;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    countdownEl.innerHTML = '$(minutes):$(seconds)';
+    time--;
+}
+//event listener
