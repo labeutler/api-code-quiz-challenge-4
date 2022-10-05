909cf143c259f3156dac1f2b0a3be48262e8c972
Laura Beutler <labeutler@gmail.com>
2022-10-05 15:37:54 -0400

    adding in style change and start of div classes for different screens once buttons are clicked.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,37 @@
 //countdown timer
-const startingMinutes = 1;
-let time = startingMinutes * 60;
+// const startingMinutes = 1;
+// let time = startingMinutes * 60;
 
-const countdownEl = document.getElementById('countdown');
+// const countdownEl = document.getElementById('countdown');
 
-setInterval(updateCountdown, 1000);
+// setInterval(updateCountdown, 1000);
 
-function updateCountdown() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+// function updateCountdown() {
+//     const minutes = Math.floor(time / 60);
+//     let seconds = time % 60;
 
-    countdownEl.innerHTML = '$(minutes):$(seconds)';
-    time--;
-}
+//     countdownEl.innerHTML = '$(minutes):$(seconds)';
+//     time--;
+// }
+
+// document.getElementById("myBtn").addEventListener("click", myFunction);
+
+// function myFunction() {
+//     countdownEl ("1:00");
+// }
 //event listener
+
+//start game
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById ('question-container')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+}
+const targetDiv = document.getElementById("second");
+
+//
