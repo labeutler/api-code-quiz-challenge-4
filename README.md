98edd9445c85ec582f58e50300073fc1a1223589
Laura Beutler <labeutler@gmail.com>
2022-10-14 10:51:08 -0400

    add images to readme

--- README.md
+++ README.md
@@ -4,32 +4,41 @@ Challenge 4 for Web APIs: Code Quiz
 ## Description
 
 Creating a game that follows the below listed Acceptance Criteria.  The game will be timed and wrong answers will be penalized by deducting time from the timer.
+Link to the game: https://labeutler.github.io/api-code-quiz-challenge-4/
+Link to my github page: https://github.com/labeutler/api-code-quiz-challenge-4/edit/main/README.md
 
 ## Acceptance Criteria
 
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and my score
+GIVEN I am taking a code quiz<br>
+WHEN I click the start button<br>
+THEN a timer starts and I am presented with a question<br>
+WHEN I answer a question<br>
+THEN I am presented with another question<br>
+WHEN I answer a question incorrectly<br>
+THEN time is subtracted from the clock<br>
+WHEN all questions are answered or the timer reaches 0<br>
+THEN the game is over<br>
+WHEN the game is over<br>
+THEN I can save my initials and my score<br>
 
 ## Installation
 
 N/A
 
-## Usage
+## Usage (NEED TO UPDATE)
 
-Provide instructions and examples for use. Include screenshots as needed.
+When clicking the link to start the game, you are presented with the following screen.
+![alt text](/assets/images/First.png)
 
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+When clicking the start button, the game begins, a question presents and the timer begins to countdown.
+![alt text](/assets/images/Timer.png)
 
-![alt text](assets/images/screenshot.png)
+When you click on an answer, the next question presents imediately after.
+![alt text](/assets/images/NxtQuest.png)
+
+When game is over, you can enter your initials.
+![alt text](/assets/images/Initial.png)
+
 
 ## Credits
 
--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
-    //start button
-const startButton = document.getElementById('start-btn')
-    //Clicking answer button will take you to next question
+   //start button
+   const startButton = document.getElementById('start-btn')
+   //Clicking answer button will take you to next question
 const answerButton = document.getElementById('answer-buttons')
-    //question container
+   //question container
 const questionContainerElement = document.getElementById ('question-container')
-    //question element
+   //question element
 const questionElement = document.getElementById('question')
-    //answer buttons element
+   //answer buttons element
 const answerButtonsElement = document.getElementById('answer-buttons')
-    //create form document
+   //create form document
 const form = document.querySelector('#highScore')
 const initials = document.querySelector('#initial');
 const score = document.querySelector('#time')
-    
+   
 
 //variable to show our shuffled and current questions
 let shuffledQuestions, currentQuestionIndex
@@ -25,47 +25,47 @@ startButton.addEventListener('click', countDown)
 
 
 function countDown() {
-    var timeleft = 60;
-        var downloadTimer = setInterval(function(){
-            if(timeleft <= 0) {
-                clearInterval(downloadTimer);
-                document.getElementById('countdown').innerHTML = 'Finished';
-            } else {
-                document.getElementById('countdown').innerHTML = timeleft + ' seconds';
-            }
-            timeleft -= 1;
-        }, 1000);
-    }
-    // document.getElementById('wrong').addEventListener('click', function() {
-    //     sec -= 5;
-    //     document.getElementById('countDown').innerHTML='00:'+sec;
-    // });
+   var timeleft = 60;
+       var downloadTimer = setInterval(function(){
+           if(timeleft <= 0) {
+               clearInterval(downloadTimer);
+               document.getElementById('countdown').innerHTML = 'Finished';
+           } else {
+               document.getElementById('countdown').innerHTML = timeleft + ' seconds';
+           }
+           timeleft -= 1;
+       }, 1000);
+   }
+   // document.getElementById('wrong').addEventListener('click', function() {
+   //     sec -= 5;
+   //     document.getElementById('countDown').innerHTML='00:'+sec;
+   // });
 
 //add event listener for answer buttons
 answerButton.addEventListener('click', function(event){
-    console.log(event)
-    if (event.target.nodeName === 'BUTTON'){
-        var choice = event.target.innerText
-        var correct = questions[currentQuestionIndex].correct
-        if (choice === correct){
-            //timer needs to be added here
-            document.getElementById('countdown').innerHTML = 'finished'
-            currentQuestionIndex++
-            //display next question
-            displayQuestion()
-        } else {
-            //timer will need to deduct time here
-            currentQuestionIndex++
-            displayQuestion()
-            // document.getElementById('wrong').addEventListener('click', function() 
-            {
-                document.getElementById('wrong').innerHTML
-                sec -= 5;
-                document.getElementById('countDown').innerHTML='00:'+sec;
-            }
-            
-        }
-    }
+   console.log(event)
+   if (event.target.nodeName === 'BUTTON'){
+       var choice = event.target.innerText
+       var correct = questions[currentQuestionIndex].correct
+       if (choice === correct){
+           //timer needs to be added here
+           document.getElementById('countdown').innerHTML = 'finished'
+           currentQuestionIndex++
+           //display next question
+           displayQuestion()
+       } else {
+           //timer will need to deduct time here
+           currentQuestionIndex++
+           displayQuestion()
+           // document.getElementById('wrong').addEventListener('click', function() 
+           {
+               document.getElementById('wrong').innerHTML
+               sec -= 5;
+               document.getElementById('countDown').innerHTML='00:'+sec;
+           }
+           
+       }
+   }
 })
 //create list for high scores
 let initial = document.getElementById('initial').value;
@@ -76,31 +76,31 @@ console.log(time);
 
 //function when the start of the game begins
 function startGame() {
-    console.log('started')
-    startButton.classList.add('hide')
-    document.getElementById('first').classList.add('hide')
-    document.getElementById('second').classList.remove('hide')
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
-    //call display question
-    displayQuestion()
+   console.log('started')
+   startButton.classList.add('hide')
+   document.getElementById('first').classList.add('hide')
+   document.getElementById('second').classList.remove('hide')
+   currentQuestionIndex = 0
+   questionContainerElement.classList.remove('hide')
+   //call display question
+   displayQuestion()
 }
 
 //display question (make function)
 function displayQuestion() {
-    //when no more questions
-    if (currentQuestionIndex === questions.length){
-        document.getElementById('second').classList.add('hide')
-        document.getElementById('third').classList.remove('hide')
-        return
-    }
-    //target something in HTML
-    document.getElementById('question').innerText = questions [currentQuestionIndex].question
-    //pull answers by targeting buttons in html
-    document.getElementById('btn1').innerText = questions [currentQuestionIndex].answer[0]
-    document.getElementById('btn2').innerText = questions [currentQuestionIndex].answer[1]
-    document.getElementById('btn3').innerText = questions [currentQuestionIndex].answer[2]
-    document.getElementById('btn4').innerText = questions [currentQuestionIndex].answer[3]
+   //when no more questions
+   if (currentQuestionIndex === questions.length){
+       document.getElementById('second').classList.add('hide')
+       document.getElementById('third').classList.remove('hide')
+       return
+   }
+   //target something in HTML
+   document.getElementById('question').innerText = questions [currentQuestionIndex].question
+   //pull answers by targeting buttons in html
+   document.getElementById('btn1').innerText = questions [currentQuestionIndex].answer[0]
+   document.getElementById('btn2').innerText = questions [currentQuestionIndex].answer[1]
+   document.getElementById('btn3').innerText = questions [currentQuestionIndex].answer[2]
+   document.getElementById('btn4').innerText = questions [currentQuestionIndex].answer[3]
 }
 
 //Create a function to submit initials for final score
@@ -123,49 +123,49 @@ function displayQuestion() {
 
 //pulls a question from our array of questions
 function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.deataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
+   questionElement.innerText = question.question
+   question.answers.forEach(answer => {
+       const button = document.createElement('button')
+       button.innerText = answer.text
+       button.classList.add('btn')
+       if (answer.correct) {
+           button.deataset.correct = answer.correct
+       }
+       button.addEventListener('click', selectAnswer)
+       answerButtonsElement.appendChild(button)
+   })
 }
 
 
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
+   clearStatusClass(element)
+   if (correct) {
+       element.classList.add('correct')
+   } else {
+       element.classList.add('wrong')
+   }
 }
 
 //create list of questions in the form of an array, with answers as another array.
 const questions = [
-    {
-        question: 'What do we use to create an aesthetically appealing web page?',
-        answer: ['A: HTML', 'B: JavaScript', 'C: CSS', 'D: Java'],
-        correct: 'C: CSS'
-    },
-    {
-        question: 'Which one of the following headings does NOT belong?',
-        answer:['A: h2', 'B: h4', 'C: h3', 'D: h8'],
-        correct: 'D: h8'
-    },
-    {
-        question: 'What is being referred to when you make a Web Design fit on any platform you are running it on?',
-        answer: ['A: Center', 'B: Justify', 'C: Responsive', 'D: Class'],
-        correct: 'C: Responsive'
-    },
-    {
-        question: 'When creating an Array, what of the following do you use?',
-        answer: ['A: ()', 'B: []', 'C: {}', 'D: <>'],
-        correct: 'B: []'
-    }
+   {
+       question: 'What do we use to create an aesthetically appealing web page?',
+       answer: ['A: HTML', 'B: JavaScript', 'C: CSS', 'D: Java'],
+       correct: 'C: CSS'
+   },
+   {
+       question: 'Which one of the following headings does NOT belong?',
+       answer:['A: h2', 'B: h4', 'C: h3', 'D: h8'],
+       correct: 'D: h8'
+   },
+   {
+       question: 'What is being referred to when you make a Web Design fit on any platform you are running it on?',
+       answer: ['A: Center', 'B: Justify', 'C: Responsive', 'D: Class'],
+       correct: 'C: Responsive'
+   },
+   {
+       question: 'When creating an Array, what of the following do you use?',
+       answer: ['A: ()', 'B: []', 'C: {}', 'D: <>'],
+       correct: 'B: []'
+   }
 ]
